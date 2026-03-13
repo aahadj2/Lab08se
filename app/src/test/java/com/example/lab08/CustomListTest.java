54c44077e459ca1e6e9ebbe9961b3831826d56db
ahadj <27100465@lums.edu.pk>
2026-03-13 23:53:45 +0500

    fixed count city

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -6,7 +6,7 @@ public class CustomListTest {
     @Test
     public void testHasCity() {
         CustomList list = new CustomList();
-        City calgary = new City("Calgary", "AB");
+        City calgary = new City("lor", "AB");
         list.addCity(calgary);
         // This will fail initially because  hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
@@ -14,7 +14,7 @@ public class CustomListTest {
     @Test
     public void testDeleteCity() {
         CustomList list = new CustomList();
-        City city = new City("Calgary", "NT");
+        City city = new City("Lahore", "NT");
         list.addCity(city);
         assertTrue(list.hasCity(city));
         list.delCity(city);
@@ -25,7 +25,7 @@ public class CustomListTest {
         CustomList list = new CustomList();
         assertEquals(0, list.countCity()); // Should be empty initially
 
-        City city = new City("Saskatoon", "SK");
+        City city = new City("Lahore", "SK");
         list.addCity(city);
 
         assertEquals(1, list.countCity());
